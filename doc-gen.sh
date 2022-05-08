528d3c51024a8adaca4e826016c8048013ef6a02
Danny Sung <danny@dannysung.com>
2022-05-08 10:10:58 -0700

    Update script based on recent official docc documentation

--- doc-gen.sh
+++ doc-gen.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
-# ref: https://www.createwithswift.com/publishing-docc-documention-as-a-static-website-on-github-pages/
+# ref: https://apple.github.io/swift-docc-plugin/documentation/swiftdoccplugin/publishing-to-github-pages/
+# ref: https://apple.github.io/swift-docc-plugin/documentation/swiftdoccplugin/generating-documentation-for-hosting-online
 
 DERIVED_DATA_DIR=/tmp/derived_data
 DOARCHIVE=${DERIVED_DATA_DIR}/Build/Products/Debug/PacketProcessor.doccarchive
 OUTPUT_DIR="docs"
 BASE_URL="SwiftPacketProcessor/"
+BUILD_TARGET="PacketProcessor"
 
-xcodebuild docbuild \
-    -scheme PacketProcessor \
-    -derivedDataPath "${DERIVED_DATA_DIR}" \
-    -destination 'platform=macOS'
 
-$(xcrun --find docc) process-archive \
-    transform-for-static-hosting "${DOARCHIVE}" \
-    --output-path "${OUTPUT_DIR}" \
-    --hosting-base-path "${BASE_URL}"
+swift package --allow-writing-to-directory "${OUTPUT_DIR}" \
+    generate-documentation --target "${BUILD_TARGET}" \
+    --disable-indexing \
+    --transform-for-static-hosting \
+    --hosting-base-path "${BASE_URL}" \
+    --output-path "${OUTPUT_DIR}"
+
 
 mv docs/index.html ${DERIVED_DATA_DIR}/index.html
 cat ${DERIVED_DATA_DIR}/index.html | sed 's/"\//"\/SwiftPacketProcessor\//g' > docs/index.html
